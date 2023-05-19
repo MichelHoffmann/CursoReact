bd640ba1ca6a94dc9dde3762cb7f35f513446360
Michel Hoffmann <mic.hoff.jr@gmail.com>
2023-05-19 07:10:19 -0500

    primeiros passos no react

--- teste/src/App.js
+++ teste/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "Michel Hoffmann",
+  };
+
+  stateClick = () => {
+    if (this.state.name === "Fui clicado")
+      this.setState({ name: "Michel Hoffmann" });
+    if (this.state.name === "Michel Hoffmann")
+      this.setState({ name: "Fui clicado" });
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 onClick={this.stateClick}>{name}</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
