f0ff2ceb28cf8165082546c31e1a3b323e2d1b8a
Michel Hoffmann <mic.hoff.jr@gmail.com>
2023-05-22 06:57:31 -0500

    continucao do projeto

--- curso/src/components/templates/Home/index.js
+++ curso/src/components/templates/Home/index.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./style.css";
 import { Component } from "react";
 import { PostCard } from "./components/PostCard";
 
@@ -29,24 +29,24 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.loadPosts()
+    this.loadPosts();
   }
 
   loadPosts = async () => {
     const postResponse = fetch("https://jsonplaceholder.typicode.com/posts");
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
 
-    const [posts, photos] = await Promise.all([postResponse, photosResponse])
+    const [posts, photos] = await Promise.all([postResponse, photosResponse]);
 
-    const postsJson = await posts.json()
-    const photosJson = await photos.json()
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
 
     const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url}
-    })
+      return { ...post, cover: photosJson[index].url };
+    });
 
-    this.setState({ posts: postsAndPhotos })
-  }
+    this.setState({ posts: postsAndPhotos });
+  };
 
   render() {
     const { posts } = this.state;
@@ -56,10 +56,10 @@ class App extends Component {
         <div className="posts">
           {posts.map((post) => (
             <PostCard
-            key={post.id}
-            title={post.title}
-            body={post.body}
-            cover={post.cover}
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              cover={post.cover}
             />
           ))}
         </div>
