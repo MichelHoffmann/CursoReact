e112c56ea09dc5d520932d1b1be30ea71814f788
Michel Hoffmann <mic.hoff.jr@gmail.com>
2023-05-22 05:49:06 -0500

    estudo da segunda-feira

--- curso/src/App.js
+++ curso/src/App.js
@@ -1,6 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
+import { PostCard } from "./components/PostCard";
 
 class App extends Component {
   constructor(props) {
@@ -29,25 +29,41 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((post) => this.setState({ posts: post }));
+    this.loadPosts()
+  }
+
+  loadPosts = async () => {
+    const postResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+
+    const [posts, photos] = await Promise.all([postResponse, photosResponse])
+
+    const postsJson = await posts.json()
+    const photosJson = await photos.json()
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url}
+    })
+
+    this.setState({ posts: postsAndPhotos })
   }
 
   render() {
     const { posts } = this.state;
+
     return (
-      <div className="container">
+      <section className="container">
         <div className="posts">
           {posts.map((post) => (
-            <div key={post.id} className="post">
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-              <hr />
-            </div>
+            <PostCard
+            key={post.id}
+            title={post.title}
+            body={post.body}
+            cover={post.cover}
+            />
           ))}
         </div>
-      </div>
+      </section>
     );
   }
 }
